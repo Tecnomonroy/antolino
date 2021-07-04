50bb19cedabd6ae2d974ff780a4c70f9831ba3cc
Tecnomonroy <86924272+Tecnomonroy@users.noreply.github.com>
2021-07-04 23:29:13 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    cuteBot.forward()
+    basic.pause(200)
+    cuteBot.motors(100, 40)
+    basic.pause(1000)
+    cuteBot.forward()
+    basic.pause(200)
+    cuteBot.motors(40, 100)
+    basic.pause(1000)
+    cuteBot.forward()
+    basic.pause(200)
+})
